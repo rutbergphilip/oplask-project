45b4d5753cf7baa96a54e2450ecf2d2c41238f98
rutbergphilip <philiprutberg00@gmail.com>
2021-02-02 21:31:22 +0100

    Added gallery and cardtemplate components

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
-export async function searchFunction(userInput) {
-    const request = await fetch(`https://api.unsplash.com/search/photos?&query=${userInput}`, {
+export async function searchFunction(userInput, pageNum) {
+    const request = await fetch(`https://api.unsplash.com/search/photos?&query=${userInput}?&per_page=9?&page=${pageNum}`, {
         headers: {
             "Authorization": `Client-ID ${process.env.VUE_APP_SECRET}`
         }
     })
     const response = await request.json()
+    console.log(response)
     return response
 }
---
+++ src/components/Gallery.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper">
+        <nav><ul><li v-for="(page, index) in pages" :key="index"></li></ul></nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
