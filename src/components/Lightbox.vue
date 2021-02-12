ed50f51874a0ac5a09026edefcea734f045fc890
dabbe01 <dabbe01@bredband.net>
2021-02-12 19:13:58 +0100

    lightbox

--- src/components/Lightbox.vue
+++ src/components/Lightbox.vue
@@ -1,7 +1,53 @@
-<template> </template>
+<template> 
+<div v-if="show" class="box" >
+    <button @click="close"> 
+        close
+    </button>
+    <div class="img" >
+        <img :src="images[index].urls.full"  alt="">
+    </div>
+</div>
+</template>
 
 <script>
-export default {};
+export default {
+    props: {
+    images: Array
+  },
+    data(){
+        return{
+            show: false,
+            index: 0
+        }
+    },
+     methods:{
+        close(){
+      this.show = false
+        }
+     }
+    
+};
 </script>
 
-<style></style>
+<style>
+.box{
+    display: flex;
+    width: 100%;
+    height: 100%;
+    background-color:black;
+    position: fixed;
+    justify-content: center;
+    align-items: center;
+    background-color:rgba(0, 0, 0, 0.7  );
+    z-index: 500;
+    top: 10;
+}
+img{
+    object-fit: cover;
+    width: 1000px;
+    height: auto;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <input type="text" v-model="searchInput" /> |
     <button @click.prevent="callback">Click Me</button>
-
+<lightbox ref="callLightbox" :images="images"/>
     <ul>
       <li v-for="(image, index) in images" :key="index">
-        <gallery :image="image" />
+        <gallery @click.native="openLightbox(index)" :image="image" />
       </li>
     </ul>
+    
+    
+
   </div>
 </template>
 
 <script>
 import Gallery from "../components/Gallery.vue";
+import Lightbox from '../components/Lightbox.vue';
 export default {
   name: "Home",
   data() {
@@ -21,11 +25,16 @@ export default {
     };
   },
   components: {
-    Gallery
+    Gallery,
+    Lightbox
   },
   methods: {
     async callback() {
       this.$store.dispatch('fetchData', this.searchInput)
+    },
+    openLightbox(index){
+      this.$refs.callLightbox.index = index
+      this.$refs.callLightbox.show = true
     }
   },
   computed: {
